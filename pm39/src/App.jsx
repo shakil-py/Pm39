7eaa3df5d8e9a71972e59644c5ce9ea318c4787d
Shakil Haque <shakilhaquepy@gmail.com>
2025-10-22 21:21:02 +0600

    add carosel

--- pm39/src/App.jsx
+++ pm39/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './component/home';
 import NavBar from './component/navbar';
 
 function App() {
@@ -6,7 +7,14 @@ function App() {
 
   return (
     <>
-      <NavBar></NavBar>
+      <div className="w-10/12 mx-auto">
+        <div className="">
+          <NavBar></NavBar>
+        </div>
+        <div className="">
+          <Home></Home>
+        </div>
+      </div>
     </>
   )
 }
